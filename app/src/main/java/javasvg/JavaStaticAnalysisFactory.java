0b392090047d8b23819d1e6a32037de74847ef89
sikimu <sikimu.pg@gmail.com>
2023-06-07 22:53:33 +0900

    セミコロンごとに分割するようにした

--- app/src/main/java/javasvg/JavaStaticAnalysisFactory.java
+++ app/src/main/java/javasvg/JavaStaticAnalysisFactory.java
@@ -3,17 +3,48 @@ package javasvg;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class JavaStaticAnalysisFactory {
   public static JavaStaticAnalysisResult create(File file) throws Exception {
-    StringBuilder sb = new StringBuilder();
+
     BufferedReader reader = new BufferedReader(new FileReader(file));
-    String line;
-    while ((line = reader.readLine()) != null) {
-      sb.append(line);
-      sb.append(System.lineSeparator());
+ 
+    // 一ブロック分ごとに文字列を格納するリスト
+    ArrayList<String> blockList = new ArrayList<String>();
+
+    String tempString = "";
+
+    int i;
+    boolean isFix = false;
+    while ((i = reader.read()) != -1) {
+
+      char c = (char) i;  
+
+      // 改行は無視する
+      if(c == '\n' || c == '\r'){
+        continue;
+      }
+
+      // 最初の文字までは無視する
+      if(c == ' ' && tempString.length() == 0){
+        continue;
+      }
+
+      tempString = tempString + c;    
+
+      if(c == ';'){
+        isFix = true;
+      }
+
+      if(isFix){
+        blockList.add(tempString);
+        tempString = "";
+        isFix = false;
+      }
     }
+
     reader.close();
-    return new JavaStaticAnalysisResult(sb.toString());
+    return new JavaStaticAnalysisResult(String.join("\r\n", blockList));
   }
 }

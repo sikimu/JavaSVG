44fcb91d6042cc7fe9c7b6a17739e1d2c29f31b0
sikimu <sikimu.pg@gmail.com>
2023-06-09 14:41:32 +0900

    最初の1文節を読み込むように変更

--- app/src/main/java/javasvg/JavaStaticAnalysisFactory.java
+++ app/src/main/java/javasvg/JavaStaticAnalysisFactory.java
@@ -3,47 +3,61 @@ package javasvg;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class JavaStaticAnalysisFactory {
-  public static JavaStaticAnalysisResultSource create(File file) throws Exception {
+  public static JavaStaticAnalysisResultSource create(File file) throws IOException {
+    String source = readFileToString(file);
+    return createSource(source, 0);
+  }
 
+  static String readFileToString(File file) throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(file));
+    StringBuilder source = new StringBuilder();
+    String line;
+    while ((line = reader.readLine()) != null) {
+      source.append(line).append(System.lineSeparator());
+    }
+    reader.close();
+    return source.toString();
+  }
+
+  static JavaStaticAnalysisResultSource createSource(String source, int index){
 
     // 解析結果(ソースコード)
-    JavaStaticAnalysisResultSource result = new JavaStaticAnalysisResultSource();
+    JavaStaticAnalysisResultSource jsarSource = new JavaStaticAnalysisResultSource();
 
-    String tempString = "";
+    // 1文節の文字列を取得
+    ArrayList<String> wordList = extractWordList(source, index);
 
-    int i;
-    boolean isFix = false;
-    while ((i = reader.read()) != -1) {
+    jsarSource.add(new JavaStaticAnalysisResultCode(wordList.toString()));
 
-      char c = (char) i;  
+    return jsarSource;
+  }
 
-      // 改行は無視する
-      if(c == '\n' || c == '\r'){
-        continue;
+  static ArrayList<String> extractWordList(String source, int index) {
+
+    ArrayList<String> wordList = new ArrayList<String>();
+
+    String word = "";
+    while (index < source.length()) {
+      char c = source.charAt(index);
+      if (c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+        if (word.length() > 0) {
+          wordList.add(word);
+          word = "";
+        }
+      } else if (c == '(' || c == ')' || c == '{' || c == '}' || c == ';') {
+        if (word.length() > 0) {
+          wordList.add(word);
+        }
+        break;
+      } else {
+        word += c;
       }
-
-      // 最初の文字までは無視する
-      if(c == ' ' && tempString.length() == 0){
-        continue;
-      }
-
-      tempString = tempString + c;    
-
-      if(c == ';'){
-        isFix = true;
-      }
-
-      if(isFix){
-        result.add(new JavaStaticAnalysisResultCode(tempString));
-        tempString = "";
-        isFix = false;
-      }
+      index++;
     }
-
-    reader.close();
-    return result;
+    return wordList;
   }
 }

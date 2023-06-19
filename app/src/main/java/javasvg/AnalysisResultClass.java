24751639ee99a4e4dfc2018e81c492ae79cb59b2
sikimu <sikimu.pg@gmail.com>
2023-06-20 00:44:58 +0900

    メソッドに対応したけどもう少しテストが必要

--- app/src/main/java/javasvg/AnalysisResultClass.java
+++ app/src/main/java/javasvg/AnalysisResultClass.java
@@ -14,7 +14,7 @@ public class AnalysisResultClass extends AnalysisResult {
         this.name = searchName(signature);
         index.increment();
 
-        this.inBraces = AnalysisResultInBraces.create(signatures, index);
+        this.inBraces = create(signatures, index);
     }
 
     private String searchName(Signature signature) {
@@ -26,5 +26,66 @@ public class AnalysisResultClass extends AnalysisResult {
             }
         }
         throw new RuntimeException("class name not found");
+    }
+
+    private AnalysisResultInBraces create(ArrayList<Signature> signatures, Index index){
+        
+        ArrayList<AnalysisResult> list = new ArrayList<AnalysisResult>();
+
+        Signature signature = signatures.get(index.get());
+
+        if (signature.contains("{") == false) {
+            throw new IllegalArgumentException("開始括弧がありません");
+        }
+        index.increment();
+
+        while (signatures.size() > index.get()) {
+            signature = signatures.get(index.get());
+
+            if (isMethod(signatures, index)) {
+                list.add(new AnalysisResultMethod(signatures, index));
+            } 
+            else if (signature.contains("{")) {
+                list.add(AnalysisResultInBraces.create(signatures, index));
+            } 
+            else if (signature.contains("(")){
+                list.add(AnalysisResultInParentheses.create(signatures, index));
+            } else if (signature.contains("}")) {
+                index.increment();
+                break;
+            } else {
+                list.add(new AnalysisResultCode(signature.toString()));
+                index.increment();
+            }            
+        }
+
+        return new AnalysisResultInBraces(list);
+    }
+
+    private boolean isMethod(ArrayList<Signature> signatures, Index index) {
+
+        if (signatures.size() <= index.get() + 1) {
+            return false;
+        }
+
+        Signature signature1 = signatures.get(index.get());
+        Signature signature2 = signatures.get(index.get() + 1);
+
+        if(signature1.contains("void") == false
+        && signature1.contains("int") == false
+        && signature1.contains("String") == false
+        && signature1.contains("double") == false
+        && signature1.contains("boolean") == false
+        && signature1.contains("char") == false
+        && signature1.contains("float") == false
+        && signature1.contains("long") == false
+        && signature1.contains("byte") == false
+        && signature1.contains("short") == false
+        && signature1.contains("Object") == false
+        && signature1.contains("ArrayList") == false){
+            return false;
+        }
+
+        return signature2.contains("(");
     }
 }
---
+++ app/src/main/java/javasvg/AnalysisResultMethod.java
@@ -0,0 +1,83 @@
+package javasvg;
+
+import java.util.ArrayList;
+
+public class AnalysisResultMethod extends AnalysisResult {
+
+    final public String name;
+
+    final public AnalysisResultCode arguments;
+
+    final public AnalysisResultInBraces inBraces;
+
+    public AnalysisResultMethod(ArrayList<Signature> signatures, Index index) {
+
+        Signature signature = signatures.get(index.get());
+        this.name = searchName(signature);
+        index.increment();
+
+        arguments = searchArguments(signatures, index);
+
+        this.inBraces = create(signatures, index);
+    }
+
+    // 引数部分の取得
+    private AnalysisResultCode searchArguments(ArrayList<Signature> signatures, Index index){
+
+        if (signatures.get(index.get()).contains("(") == false) {
+            throw new IllegalArgumentException("開始括弧がありません");
+        }
+        index.increment();
+        
+        Signature signature = signatures.get(index.get());
+        index.increment();
+        // 引数がない場合
+        if (signature.contains(")")) {
+            return new AnalysisResultCode("");
+        }   
+        
+        // 引数がある場合
+        AnalysisResultCode result = new AnalysisResultCode(signature.toString());
+        if (signatures.get(index.get() + 2).contains(")") == false) {
+            throw new IllegalArgumentException("終了括弧がありません");
+        }                
+        index.increment();
+        return result;
+    }
+
+    private String searchName(Signature signature) {
+
+        return signature.get(signature.size() - 1);
+    }
+
+    private AnalysisResultInBraces create(ArrayList<Signature> signatures, Index index){
+        
+        ArrayList<AnalysisResult> list = new ArrayList<AnalysisResult>();
+
+        Signature signature = signatures.get(index.get());
+
+        if (signature.contains("{") == false) {
+            throw new IllegalArgumentException("開始括弧がありません");
+        }
+        index.increment();
+
+        while (signatures.size() > index.get()) {
+            signature = signatures.get(index.get());
+
+            if (signature.contains("{")) {
+                list.add(AnalysisResultInBraces.create(signatures, index));
+            } 
+            else if (signature.contains("(")){
+                list.add(AnalysisResultInParentheses.create(signatures, index));
+            } else if (signature.contains("}")) {
+                index.increment();
+                break;
+            } else {
+                list.add(new AnalysisResultCode(signature.toString()));
+                index.increment();
+            }            
+        }
+
+        return new AnalysisResultInBraces(list);
+    }
+}

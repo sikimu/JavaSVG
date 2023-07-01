23fca7e6e2e5bb6ed35d89444d70e152feb21f00
sikimu <sikimu.pg@gmail.com>
2023-07-01 22:58:59 +0900

    メソッドの()は自動で読み込まないようにした

--- app/src/main/java/javasvg/AnalysisResultMethod.java
+++ app/src/main/java/javasvg/AnalysisResultMethod.java
@@ -87,9 +87,7 @@ public class AnalysisResultMethod extends AnalysisResult {
             if (signature.contains("{")) {
                 list.add(AnalysisResultInBraces.create(signatures, index));
             } 
-            else if (signature.contains("(")){
-                list.add(AnalysisResultInParentheses.create(signatures, index));
-            } else if (signature.contains("}")) {
+            else if (signature.contains("}")) {
                 index.increment();
                 break;
             } else {

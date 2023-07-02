5f9eb0b283186c2786d7b75ff65985d9dd028397
sikimu <sikimu.pg@gmail.com>
2023-07-02 22:48:37 +0900

    変数名変更

--- app/src/main/java/jp/co/javanalysis/analysis/AnalysisResultCode.java
+++ app/src/main/java/jp/co/javanalysis/analysis/AnalysisResultCode.java
@@ -7,7 +7,7 @@ import jp.co.javanalysis.phrase.Phrase;
 public class AnalysisResultCode extends AnalysisResult {
 
     String destination;
-    String source;
+    String expression;
     String code;
 
     public AnalysisResultCode(Phrase phrase) {
@@ -18,9 +18,9 @@ public class AnalysisResultCode extends AnalysisResult {
         if(phrase.contains("=")) {
             List<Phrase> split = phrase.split("=");
             destination = split.get(0).toString();
-            source = split.get(1).toString();
+            expression = split.get(1).toString();
         } else {
-            source = phrase.toString();
+            expression = phrase.toString();
         }
     }
 

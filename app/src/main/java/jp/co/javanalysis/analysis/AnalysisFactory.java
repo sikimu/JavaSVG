2bd396c16557d133d117d4bc92777492e6993db7
sikimu <sikimu.pg@gmail.com>
2023-07-06 22:35:08 +0900

    File読み込み処理を分離

--- app/src/main/java/jp/co/javanalysis/analysis/AnalysisFactory.java
+++ app/src/main/java/jp/co/javanalysis/analysis/AnalysisFactory.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javasvg.Index;
+import jp.co.javanalysis.FileUtil;
 import jp.co.javanalysis.phrase.Phrase;
 import jp.co.javanalysis.phrase.PhraseListFactory;
 
@@ -17,7 +18,7 @@ public class AnalysisFactory {
   public static AnalysisResultPackage createPackage(String folderPath) throws Exception {
 
     // folderPath内のファイルを再帰的に取得
-    List<File> fileList = getAllFiles(folderPath);
+    List<File> fileList = FileUtil.getAllFiles(folderPath);
 
     ArrayList<AnalysisResultSource> list = new ArrayList<AnalysisResultSource>();
 
@@ -33,44 +34,13 @@ public class AnalysisFactory {
     return new AnalysisResultPackage(list);
   }
 
-  /**
-   * 再帰的にファイルを取得する
-   * 
-   * @param folderPath
-   * @throws Exception
-   */
-  private static List<File> getAllFiles(String folderPath) throws Exception {
-    List<File> fileList = new ArrayList<File>();
-    File[] files = new File(folderPath).listFiles();
-    for (int i = 0; i < files.length; i++) {
-      File file = files[i];
-      if (file.isDirectory()) {
-        fileList.addAll(getAllFiles(file.getPath()));
-      } else {
-        fileList.add(file);
-      }
-    }
-    return fileList;
-  }
-
   public static AnalysisResultSource create(File file) throws IOException {
-    String source = readFileToString(file);
+    String source = FileUtil.readFileToString(file);
     return create(source);
   }
 
   public static AnalysisResultSource create(String source) {
     return createSource(PhraseListFactory.create(source));
-  }
-
-  static String readFileToString(File file) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-    StringBuilder source = new StringBuilder();
-    String line;
-    while ((line = reader.readLine()) != null) {
-      source.append(line).append(System.lineSeparator());
-    }
-    reader.close();
-    return source.toString();
   }
 
   /**

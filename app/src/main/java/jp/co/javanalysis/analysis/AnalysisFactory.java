3674fb87db91c0fbd59b8052a8d9360a9e999cba
sikimu <sikimu.pg@gmail.com>
2023-07-06 22:53:29 +0900

    だからimportもれ。。。

--- app/src/main/java/jp/co/javanalysis/analysis/AnalysisFactory.java
+++ app/src/main/java/jp/co/javanalysis/analysis/AnalysisFactory.java
@@ -3,7 +3,6 @@ package jp.co.javanalysis.analysis;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import javasvg.Index;
 import jp.co.javanalysis.FileUtil;

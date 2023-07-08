74bbcc1f4f50ec9fe62f51226b9855ddc8f93aac
sikimu <sikimu.pg@gmail.com>
2023-07-08 21:52:34 +0900

    Codeは今後廃止予定

--- app/src/main/java/jp/co/javanalysis/analysis/AnalysisResultCode.java
+++ app/src/main/java/jp/co/javanalysis/analysis/AnalysisResultCode.java
@@ -1,28 +1,14 @@
 package jp.co.javanalysis.analysis;
 
-import java.util.List;
-
-import jp.co.javanalysis.analysis.code.CommandCode;
 import jp.co.javanalysis.phrase.Phrase;
 
 public class AnalysisResultCode extends AnalysisResult {
 
-    AnalysisResultVariable destination;
-    CommandCode command;
     String code;
 
     public AnalysisResultCode(Phrase phrase) {
 
         code = phrase.toString();
-
-        //代入文の場合
-        if(phrase.contains("=")) {
-            List<Phrase> split = phrase.split("=");
-            destination = new AnalysisResultVariable(split.get(0));
-            command = new CommandCode(split.get(1));
-        } else {
-            command = new CommandCode(phrase);
-        }
     }
 
     public String toString() {
